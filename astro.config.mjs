b0327e3ff379706c8e93e40394250734b7c6e5ac
Frederic Vanhoolant <vanhoolantfrederic@hotmail.com>
2026-01-06 14:56:01 +0100

    Jewed

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com', // TODO: Remplacer par l'URL du site
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
+  server: {
+    host: '0.0.0.0',
+    port: 4321
+  },
+  integrations: [react(), markdoc(), sitemap()],
+  prefetch: true,
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'always',
+  },
+});
