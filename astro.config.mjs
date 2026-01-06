8a5e7b7acea198f909a69babb805cc8ae8499c0d
Frederic Vanhoolant <vanhoolantfrederic@hotmail.com>
2026-01-06 15:08:28 +0100

    Vecel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // TODO: Remplacer par l'URL du site
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  server: {
-    host: '0.0.0.0',
-    port: 4321
-  },
+  site: 'https://votre-portfolio.vercel.app', // TODO: Remplacer par votre URL finale Vercel
+  output: 'static', // Mode statique pour une performance maximale sur Vercel
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [react(), markdoc(), sitemap()],
   prefetch: true,
   compressHTML: true,
